0b0278b07eb8140b91127048d3a6550224bf1f5d
zhangle <zhangle@cmss.chinamobile.com>
2019-10-23 10:55:53 +0800

    jedis 实现分布式锁 redis单机版

---
+++ jedis-demo/src/test/java/com/kirago/jedis/demo/JedisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirago.jedis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JedisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
